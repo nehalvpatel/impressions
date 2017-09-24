27272b1d710e5180f19665d9befaede3cb05141b
Vasant <Vasant@Nehals-MacBook-Pro-2.local>
2017-09-23 23:31:55 -0500

    Fix UAH scraper

--- uah.php
+++ uah.php
@@ -4,21 +4,12 @@ use Goutte\Client;
 
 $uahEvents = [];
 
-function gatherUAHEvents() {
-    global $uahEvents;
-
-$client = new Client();
-
-$crawler = $client->request('GET', 'http://www.uah.edu/events');
-
-$uahEvents = [];
-
-$crawler->filter(".ev_td_li")->each(function ($node) {
+function addEvent($node, $month, $day) {
     global $uahEvents;
 
     $title = trim($node->filter(".jevents-title")->extract("_text")[0]);
     $location = trim($node->filter(".jevents-event-venue")->extract("_text")[0]);
-    $monthasdf = trim($node->filter(".jevents-event-date")->extract("_text")[0]);
+    
     $url = "http://www.uah.edu" . trim($node->filter("a")->extract("href")[0]);
     $timeStart = trim($node->filter(".jevents-eventlist-starttime")->extract("_text")[0]);
     $images = $node->filter(".bkgimage")->extract("style")[0];
@@ -38,14 +29,6 @@ $crawler->filter(".ev_td_li")->each(function ($node) {
         }
     }
 
-    if (strpos($monthasdf, "-") !== false) {
-        
-    } else {
-        $date2 = explode(" ", trim($monthasdf));
-        
-        $month = $date2[0];
-        $day = $date2[1];
-
     $event = [
         "source" => "UAHEvents",
         "title" => $title,
@@ -62,6 +45,38 @@ $crawler->filter(".ev_td_li")->each(function ($node) {
 
     $uahEvents[] = $event;
 }
+
+function gatherUAHEvents() {
+
+    global $uahEvents;
+$client = new Client();
+
+$crawler = $client->request('GET', 'http://www.uah.edu/events');
+
+// $uahEvents = [];
+
+$crawler->filter(".ev_td_li")->each(function ($node) {
+    global $uahEvents;
+    $monthasdf = trim($node->filter(".jevents-event-date")->extract("_text")[0]);
+
+    if (strpos($monthasdf, "-") !== false) {
+        $DATESPLIT = explode("-", $monthasdf);
+        $endDateSplit = explode(" ", trim($DATESPLIT[1]));
+
+        $startDate = DateTime::createFromFormat("M d", trim($DATESPLIT[0]));
+        $endDate = DateTime::createFromFormat("M d", $endDateSplit[0] . " " . $endDateSplit[1]);
+        $endDate->modify('+1 day'); 
+
+        $interval = DateInterval::createFromDateString('1 day');
+        $period = new DatePeriod($startDate, $interval, $endDate);
+        
+        foreach ( $period as $dt ) {
+            addEvent($node, $dt->format("M"), $dt->format("d"));
+        }        
+    } else {
+        $date2 = explode(" ", trim($monthasdf));
+        addEvent($node, $date2[0], $date2[1]);
+    }
 });
     return $uahEvents;
 }
